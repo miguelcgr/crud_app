6527d1b539580e62a2295e090a919503d8bd705b
miguel <miguelcalvoguti@gmail.com>
2021-08-24 15:52:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from 'axios';
 
 function App() {
+
+  const [adTitle, setAdTitle] = useState("");
+  const [adText, setAdText] = useState("");
+  console.log('adtitle', adTitle)
+  console.log('adtext', adText)
+
+  const submitAdText = () => {
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD APPLICATION</h1>
+
+      <div className="form">
+        <label>Title</label>
+        <input type="text" name="Title" onChange ={(e) => {setAdTitle(e.target.value)}}></input>
+        <label>Text</label>
+        <input type="text" name="Text" onChange ={(e) => {submitAdText(e.target.value)}}></input>
+        <button>Post</button>
+      </div>
     </div>
   );
 }
